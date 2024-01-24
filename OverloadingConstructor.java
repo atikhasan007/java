6f443ceea9a92186e7ff0baa9f76c17ca893e6aa
Md. Atik hasan <109493827+atikhasan007@users.noreply.github.com>
2024-01-24 20:04:25 +0600

    Create OverloadingConstructor.java

---
+++ OverloadingConstructor.java
@@ -0,0 +1,14 @@
+class OverloadingConstructor{
+int id;
+String name;
+int age;
+public OverloadingConstructor(int x, String y) {id=x;name=y;}
+public OverloadingConstructor(int x, String y, int z) {id=x;name=y;age=z;}
+void show(){System.out.println(id+" "+name+" "+age);}
+public static void main(String [] args)
+{
+OverloadingConstructor c1 = new OverloadingConstructor(101,"Sanowar");
+OverloadingConstructor c2 = new OverloadingConstructor(102,"Anayet",22);
+c1.show();
+c2.show();
+}}
